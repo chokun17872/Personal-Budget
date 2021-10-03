d9320c1a11db7d6ba82cfa372e945ee4b7a4748c
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2021-10-03 12:34:24 +0700

    add database

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const budget = 10000;
+const envelopes = [
+    {
+        id: 1,
+        name: 'food',
+        fund : 3500
+    },
+    {
+        id: 2,
+        name: 'education',
+        fund: 2500
+    },
+    {
+        id: 3,
+        name: 'transportation',
+        fund : 1500
+    },
+    {
+        id: 4,
+        name: 'videoGames',
+        fund : 1500
+    },
+    {
+        id: 5,
+        name: 'utilities',
+        fund: 1000
+    }
+];
 
 app.get('/',(req,res,next) => {                   
-    res.send('Hello world!');
+    res.send(envelopes);
 })
 
 app.listen(PORT, () => {
